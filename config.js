35f9098588c063a85cbde324d3889abfa123907c
S.Hacker <s.hacker.pk@gmail.com>
2025-07-28 09:59:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~h2cnlLSD#3pNkwLoXmGeW0F-xj76ilurJsglg0cb2N2p5cSc6ABw",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
